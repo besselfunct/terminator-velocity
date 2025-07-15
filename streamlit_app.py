ce9c4b64b543466f8239b404fff7971a89b50c69
Besselfunct <51245782+besselfunct@users.noreply.github.com>
2025-07-15 08:57:47 -0700

    Initial spheroid logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,21 @@ a = 6378137 # meters
 b = 6356752.314245 # meters
 f = 1/298.257223563
 
-def radius(theta, a, b):
+def find_radius(theta, a, b):
   r = ((b - a)/(np.pi/2)) * theta + a
+  return r
 
+def find_circumference_radius(theta, radius):
+  circum_radius = np.cos(theta) * radius
+  return circum_radius
+
+theta_range = np.arange(0, np.pi/2, 10 * np.pi/180)
+
+accumulator = []
+for angle in theta_range:
+  r = find_radius(angle, a, b)
+  circum_radius = find_circumference_radius(angle, r)
+  circumference = 2 * np.pi * circum_radius
+  accumulator.append(circumference)
+
+st.print(accumulator)
