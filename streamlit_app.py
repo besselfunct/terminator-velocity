9a893062066c721ca1dad5b1db7feb345f56bb14
Besselfunct <51245782+besselfunct@users.noreply.github.com>
2025-07-15 08:50:35 -0700

    Add physical constants

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 st.title("Terminator Velocity")
+
+a = 6378137 # meters
+b = 6356752.314245 # meters
+f = 1/298.257223563
+
+def radius(theta, a, b):
+  r = ((b - a)/(np.pi/2)) * theta + a
+
